6eabf4b0e53c21e810cf654c9f5d75e0b37490df
sheriff777 <aibek.syd@gmail.com>
2020-01-30 00:16:30 +0600

    2

---
+++ src/test/java/CheckServiceTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+class CheckServiceTest {
+
+    @Test
+    public void shouldSubmitRequest() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Василий");
+        elements.get(1).sendKeys("+79270000000");
+        driver.findElement(By.className("checkbox__control")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("alert-success")).getText();
+        assertEquals("Ваша заявка успешно отправлена!", text.trim());
+    }
+}
