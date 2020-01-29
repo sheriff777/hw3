dd3a2a3b5badc8bd6c1681d84cd5dc8b65d2ea75
sheriff777 <aibek.syd@gmail.com>
2020-01-30 00:19:37 +0600

    4

--- src/test/java/CheckServiceTest.java
+++ src/test/java/CheckServiceTest.java
@@ -12,12 +12,5 @@ class CheckServiceTest {
     @Test
     public void shouldSubmitRequest() {
         driver.get("http://localhost:9999");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Василий");
-        elements.get(1).sendKeys("+79270000000");
-        driver.findElement(By.className("checkbox__control")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("alert-success")).getText();
-        assertEquals("Ваша заявка успешно отправлена!", text.trim());
     }
 }
