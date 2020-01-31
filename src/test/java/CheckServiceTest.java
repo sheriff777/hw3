3e211771e1d6d9cd6a7e6f3a2ff94608e3302f3c
sheriff777 <aibek.syd@gmail.com>
2020-01-31 23:10:17 +0600

    5

--- src/test/java/CheckServiceTest.java
+++ src/test/java/CheckServiceTest.java
@@ -1,13 +1,14 @@
-import org.junit.Test;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.Test;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-
 class CheckServiceTest {
+
+    @Test
 
     @Test
     public void shouldSubmitRequest() {
