c2856e15fa89c6d468ea4b4fd6e8946e984324a9
sheriff777 <aibek.syd@gmail.com>
2020-02-01 02:21:48 +0600

    15

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -7,17 +7,30 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class CallbackTest {
+
     @Test
-    void shouldTest() {
+    void shouldTestYes() {
         open("http://localhost:9999");
         SelenideElement form = $("form.form");
-        form.$("[data-test-id=name] input").setValue("Василий Иванов-Петров");
-        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=name] input").setValue("Сыдыгалиев Айбек");
+        form.$("[data-test-id=phone] input").setValue("+78745002587");
         form.$("[data-test-id=agreement]").click();
         form.$("[type=button]").click();
         $("[data-test-id=order-success]")
                 .shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.")
                 );
+    }
+
+    @Test
+    public void shouldTestNo() {
+        open("http://localhost:9999");
+        SelenideElement form = $("form.form");
+        form.$("[data-test-id=name] input").setValue("Sydygaliev Aibek");
+        form.$("[data-test-id=phone] input").setValue("+78745002587");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave
+                (exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 }
 
